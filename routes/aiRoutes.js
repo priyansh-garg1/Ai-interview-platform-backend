710731e83918cc065df80439ce29cf5ac8409390
priyansh-garg1 <priyanshgarg@Priyanshs-MacBook-Air.local>
2025-06-22 16:40:33 +0530

    Added Feedback

--- routes/aiRoutes.js
+++ routes/aiRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { generateAIResponse } from '../controllers/aiController.js';
+import { getInterviewFeedback } from '../controllers/aiController.js';
 
 const router = Router();
 
 router.post('/generate', generateAIResponse);
+router.post('/feedback', getInterviewFeedback);
 
 export default router;
