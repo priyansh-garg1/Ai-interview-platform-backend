c2c7c867413ae3fd39257f8c408592a1d8dcd29c
Priyansh <priyanshgarg382@gmail.com>
2025-06-22 16:42:08 +0530

    Added Feedback

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { QUESTIONS_PROMPT } from '../constants/aiConstants.js';
+import { FEEDBACK_PROMPT, QUESTIONS_PROMPT } from '../constants/aiConstants.js';
 
 export const generateAIResponse = async (req, res) => {
   const { jobPosition, jobDescription, type, duration } = req.body;
@@ -32,3 +32,31 @@ export const generateAIResponse = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const getInterviewFeedback = async (req, res) => {
+    const { conversation } = req.body;
+    console.log(conversation);
+    const FINAL_PROMPT = FEEDBACK_PROMPT.replace("{{conversation}}", JSON.stringify(conversation));
+  try {
+    const openai = new OpenAI({
+      baseURL: "https://openrouter.ai/api/v1",
+      apiKey: process.env.OPENROUTER_API_KEY,
+    });
+
+    const completion = await openai.chat.completions.create({
+      model: "google/gemini-2.0-flash-exp:free",
+      messages: [
+        {
+          role: "user",
+          content: FINAL_PROMPT,
+        },
+      ],
+    });
+    console.log(completion.choices[0].message.content);
+
+    return res.json(completion.choices[0].message.content);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
